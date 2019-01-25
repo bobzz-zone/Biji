d12e88fd8de775fe9ff6193e2f6c6b3c7ca7568b
bobzz-zone <bobzz.zone@gmail.com>
2019-01-25 13:34:54 +0700

    js

--- penerimaan_biji/penerimaan_biji/doctype/data_produksi/data_produksi.js
+++ penerimaan_biji/penerimaan_biji/doctype/data_produksi/data_produksi.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2019, PT DAS and contributors
 // For license information, please see license.txt
-cur_frm.add_fetch("id_alat", "kode_objek", "kode_objek");
-cur_frm.add_fetch("id_alat", "unit_produksi", "unit_produksi");
-cur_frm.add_fetch("id_alat", "badan_usaha", "badan_usaha");
+cur_frm.add_fetch("batch", "objek_produksi", "kode_objek");
+cur_frm.add_fetch("batch", "unit_produksi", "unit_produksi");
+cur_frm.add_fetch("batch", "badan_usaha", "badan_usaha");
+cur_frm.add_fetch("batch", "kelas", "kelas");
 cur_frm.add_fetch("batch", "satuan", "satuan");
 cur_frm.add_fetch("batch", "id_alat", "id_alat");
 
--- penerimaan_biji/penerimaan_biji/doctype/kode_batch/kode_batch.js
+++ penerimaan_biji/penerimaan_biji/doctype/kode_batch/kode_batch.js
@@ -1,6 +1,9 @@
 // Copyright (c) 2019, PT DAS and contributors
 // For license information, please see license.txt
-
+cur_frm.add_fetch("id_alat", "kode_objek", "objek_produksi");
+cur_frm.add_fetch("id_alat", "unit_produksi", "unit_produksi");
+cur_frm.add_fetch("id_alat", "badan_usaha", "badan_usaha");
+cur_frm.add_fetch("id_alat", "kelas", "kelas");
 frappe.ui.form.on('Kode Batch', {
 	refresh: function(frm) {
 
