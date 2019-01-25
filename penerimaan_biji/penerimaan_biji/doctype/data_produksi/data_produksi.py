fdb12968b11f83bfbbce82ecc14fcc9d4107f90a
bobzz-zone <bobzz.zone@gmail.com>
2019-01-25 12:47:23 +0700

    dp

--- penerimaan_biji/penerimaan_biji/doctype/data_produksi/data_produksi.py
+++ penerimaan_biji/penerimaan_biji/doctype/data_produksi/data_produksi.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class DialyData(Document):
+class DataProduksi(Document):
 	pass
 	def on_submit(self):
 		batch = frappe.get_doc("Kode Batch",self.batch)
--- penerimaan_biji/penerimaan_biji/doctype/id_alat/id_alat.py
+++ penerimaan_biji/penerimaan_biji/doctype/id_alat/id_alat.py
@@ -7,4 +7,5 @@ import frappe
 from frappe.model.document import Document
 
 class IDAlat(Document):
-	pass
+	def autoname(self):
+		return "{}{}{}{}".format(self.kode_objek,self.kode_alat,self.id_alat,self.iup)
