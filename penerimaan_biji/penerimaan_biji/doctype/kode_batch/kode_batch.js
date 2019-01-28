a41e4ebd38c02a19e6d353664e955e156b01b371
bobzz-zone <bobzz.zone@gmail.com>
2019-01-29 00:25:02 +0700

    a

--- penerimaan_biji/penerimaan_biji/doctype/kode_batch/kode_batch.js
+++ penerimaan_biji/penerimaan_biji/doctype/kode_batch/kode_batch.js
@@ -4,6 +4,7 @@ cur_frm.add_fetch("id_alat", "kode_objek", "objek_produksi");
 cur_frm.add_fetch("id_alat", "unit_produksi", "unit_produksi");
 cur_frm.add_fetch("id_alat", "badan_usaha", "badan_usaha");
 cur_frm.add_fetch("id_alat", "kelas", "kelas");
+cur_frm.add_fetch("id_alat", "s_loc", "s_loc");
 frappe.ui.form.on('Kode Batch', {
 	refresh: function(frm) {
 
