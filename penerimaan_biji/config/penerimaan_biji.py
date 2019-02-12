d7832251a02cc8004b72eb9d0e947a1f6491d50f
bobzz-zone <bobzz.zone@gmail.com>
2019-02-12 09:49:59 +0700

    Update penerimaan_biji.py

--- penerimaan_biji/config/penerimaan_biji.py
+++ penerimaan_biji/config/penerimaan_biji.py
@@ -11,7 +11,7 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Mutasi Biji",
+					"name": "Mutasi Bijih",
 				},
 				{
 					"type": "doctype",
@@ -23,7 +23,7 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Pengolahan Biji",
+					"name": "Pengolahan Bijih",
 				},
 				{
 					"type": "doctype",
