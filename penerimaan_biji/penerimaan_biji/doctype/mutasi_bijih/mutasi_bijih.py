e0dfd3916eb279c43bb87b162cbef5b306a23e24
bobzz-zone <bobzz.zone@gmail.com>
2019-02-20 22:14:30 +0700

    Update mutasi_bijih.py

--- penerimaan_biji/penerimaan_biji/doctype/mutasi_bijih/mutasi_bijih.py
+++ penerimaan_biji/penerimaan_biji/doctype/mutasi_bijih/mutasi_bijih.py
@@ -27,27 +27,29 @@ class MutasiBijih(Document):
 		if last == self.aktivitas:
 			frappe.throw("Tidak bisa melakukan {} karena belum {}".format(last,reason))
 	def on_submit(self):
-		batch=frappe.get_doc("Kode Batch",self.batch)
-		if not batch.sn_tak:
-			if self.sn_tak :
-				batch.sn_tak = self.berat_ore * self.sn_tak / self.qty
-				batch.tak_percent = self.sn_tak
-		if not batch.sn_def:
-			if self.sn_def:
-				batch.sn_def = self.berat_ore * self.sn_def / self.qty
-				batch.def_percent = self.sn_def
-		batch.ton_final=self.total
-		if self.type == "PPBT" or self.type == "BPM" :
-			batch.ppbt_total = self.total
-		elif self.type == "GBT":
-			batch.gbt_total = self.total
-		elif self.type == "GMP":
-			batch.gmp_total = self.total
-		batch.update()
-		list_dp = frappe.db.sql("""select name , qty from `tabData Produksi` where batch="{}" and (sn_def is NULL or sn_def=0) and (sn_tak is NULL or sn_tak=0) """.format(self.batch),as_list=1)
-		for row in list_dp:
-			kadar = batch.sn_tak
-			if self.sn_def:
-				kadar=batch.sn_def
-			frappe.db.sql("""update `tabData Produksi` set sn_tak="{}" , sn_def="{}" , total="{}" where name={} """.format(self.sn_tak,self.sn_def,cint(row[1])*kadar,row[0]))
+		if self.aktivitas == "Penerimaan":
+			batch=frappe.get_doc("Kode Batch",self.batch)
+			if not batch.sn_tak:
+				if self.sn_tak :
+					batch.sn_tak = self.berat_ore * self.sn_tak / self.qty
+					batch.tak_percent = self.sn_tak
+			if not batch.sn_def:
+				if self.sn_def:
+					batch.sn_def = self.berat_ore * self.sn_def / self.qty
+					batch.def_percent = self.sn_def
+			batch.ton_final=self.total
+			if self.type == "PPBT" or self.type == "BPM" :
+				batch.ppbt_total = self.total
+			elif self.type == "GBT":
+				batch.gbt_total = self.total
+			elif self.type == "GMP":
+				batch.gmp_total = self.total
+			batch.s_loc = self.to_loc
+			batch.update()
+			list_dp = frappe.db.sql("""select name , qty from `tabData Produksi` where batch="{}" and (sn_def is NULL or sn_def=0) and (sn_tak is NULL or sn_tak=0) """.format(self.batch),as_list=1)
+			for row in list_dp:
+				kadar = batch.sn_tak
+				if self.sn_def:
+					kadar=batch.sn_def
+				frappe.db.sql("""update `tabData Produksi` set sn_tak="{}" , sn_def="{}" , total="{}" where name={} """.format(self.sn_tak,self.sn_def,cint(row[1])*kadar,row[0]))
 
