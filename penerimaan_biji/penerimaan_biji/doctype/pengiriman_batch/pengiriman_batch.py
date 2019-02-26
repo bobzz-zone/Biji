10196e12662d6844f741f419a4b42ebde0b65213
bobzz-zone <bobzz.zone@gmail.com>
2019-02-26 16:03:34 +0700

    Update pengiriman_batch.py

--- penerimaan_biji/penerimaan_biji/doctype/pengiriman_batch/pengiriman_batch.py
+++ penerimaan_biji/penerimaan_biji/doctype/pengiriman_batch/pengiriman_batch.py
@@ -9,10 +9,13 @@ from frappe.model.document import Document
 class PengirimanBatch(Document):
 	pass
 	def validate(self):
+		total = 0
 		for row in self.batch_list:
 			batch=frappe.get_doc("Kode Batch",row.batch)
+			total = row.input
 			if batch.jalan==1 or batch.used==1 or batch.status=="Open":
 				frappe.throw("{} Tidak bisa di lakukan pengiriman".fomat(batch.name))
+		self.input=total
 		
 	def on_cancel(self):
 		frappe.throw("Dokumen tidak dapat di batalkan")
