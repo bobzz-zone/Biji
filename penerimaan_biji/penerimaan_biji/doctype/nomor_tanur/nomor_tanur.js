492cd3912a6112630869cf67b0c5cb26c14caa8f
root <bobzz.zone@gmail.com>
2019-03-05 05:28:05 +0000

    aa

---
+++ penerimaan_biji/penerimaan_biji/doctype/nomor_tanur/nomor_tanur.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, PT DAS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Nomor Tanur', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ penerimaan_biji/penerimaan_biji/doctype/smelter/smelter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, PT DAS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Smelter', {
+	refresh: function(frm) {
+
+	}
+});
