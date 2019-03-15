df849c8abc1b8d88d4c318483108ab3ef7f2cc3b
bobzz-zone <bobzz.zone@gmail.com>
2019-03-15 13:53:39 +0700

    Update data_produksi.py

--- penerimaan_biji/penerimaan_biji/doctype/data_produksi/data_produksi.py
+++ penerimaan_biji/penerimaan_biji/doctype/data_produksi/data_produksi.py
@@ -27,7 +27,7 @@ class DataProduksi(Document):
 		batch.ton_tak=batch.sn_tak*batch.total
 		batch.ton_est=batch.sn_est*batch.total
 		batch.calculate()
-		batch.closing=now()
+		batch.closing=self.date
 		batch.save(ignore_permissions=True)
 	def on_delete(self):
 		batch = frappe.get_doc("Kode Batch",self.batch)
